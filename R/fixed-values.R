a9deb3d3a4bd4933ff7cd2c606b38c4388ba4255
”AnFeliz” <anfeliz@phoenixintnl.com>
2022-07-13 09:04:45 -0400

    Exploring protest per continent

--- R/fixed-values.R
+++
@@ -1,8 +0,0 @@
-
-words_after_number <- c("people","protesters","drivers","residents","supporters",
-                        "members","participants","former","demonstrators") %>%
-  str_c(collapse = "|")
-
-
-words_before_number <- c("about","around","more than",">","<","almost","over","more than") %>%
-  str_c(collapse = "|")
